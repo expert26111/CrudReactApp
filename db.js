d30954a633f8000c8222ae9422a33942e37cf605
expert26111 <djoana.v@gmail.com>
2017-10-13 17:22:14 +0200

    adding more staff

--- db.js
+++ db.js
@@ -39,7 +39,7 @@ var state = {
 
 exports.connect = function(mode, done) {
     state.pool = mysql.createPool({
-        host: '46.101.251.32',
+        host: '46.101.251.32:3306',
         user: 'root',
         password: 'root',
         database: mode === exports.MODE_PRODUCTION ? PRODUCTION_DB : TEST_DB
