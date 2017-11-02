87eeb0657babe8b73e1d478d63fce42126e43b2d
expert26111 <djoana.v@gmail.com>
2017-11-02 15:30:01 +0100

    added db config for heroku

--- db.js
+++ db.js
@@ -4,7 +4,9 @@
 var mysql = require('mysql')
     , async = require('async')
 
-var PRODUCTION_DB = 'app_prod_database'
+// mysql://b5be55e5b20b7f:e5f589ee@us-cdbr-iron-east-05.cleardb.net/heroku_1ed23376d98b2e2?reconnect=true
+
+var PRODUCTION_DB = 'heroku_1ed23376d98b2e2'
     , TEST_DB = 'novipel'
 
 exports.MODE_TEST = 'mode_test'
@@ -14,6 +16,33 @@ var state = {
     pool: null,
     mode: null,
 }
+
+exports.connect = function(mode, done) {
+    console.log(" inside connect method ");
+    state.pool = mysql.createPool({
+        host: 'us-cdbr-iron-east-05.cleardb.net',
+        user: 'b5be55e5b20b7f',
+        password: 'e5f589ee',
+        database: mode === exports.MODE_PRODUCTION ? PRODUCTION_DB : TEST_DB
+    })
+    state.mode = mode
+    done()
+}
+
+//LOCAL
+// exports.connect = function(mode, done) {
+//     console.log(" inside connect method ");
+//     state.pool = mysql.createPool({
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'root',
+//         database: mode === exports.MODE_PRODUCTION ? PRODUCTION_DB : TEST_DB
+//     })
+//     state.mode = mode
+//     done()
+// }
+
+
 
 // exports.connect = function(done) {
 //     state.pool = mysql.createPool({
@@ -38,17 +67,6 @@ var state = {
 // }
 
 
-exports.connect = function(mode, done) {
-    console.log(" inside connect method ");
-    state.pool = mysql.createPool({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: mode === exports.MODE_PRODUCTION ? PRODUCTION_DB : TEST_DB
-    })
-    state.mode = mode
-    done()
-}
 
 
 exports.get = function() {
