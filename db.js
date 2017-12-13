7023c4499a7851c4cd1fe9ac6e816c1fcf9a2b0c
expert26111 <djoana.v@gmail.com>
2017-12-13 16:43:10 +0100

    changed host

--- db.js
+++ db.js
@@ -46,7 +46,7 @@ var state = {
 exports.connect = function(mode, done) {
     console.log(" inside connect method ");
     state.pool = mysql.createPool({
-        host: 'localhost',
+        host: '172.18.0.1',
         user: 'root',
         password: 'root',
         database: mode === exports.MODE_PRODUCTION ? PRODUCTION_DB : TEST_DB
